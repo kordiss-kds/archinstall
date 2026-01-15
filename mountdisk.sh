a2d491f7cc9fcf493fc45f4d7916c8301efb37d5
kordiss-kds <720074@gmail.com>
2026-01-16 01:32:11 +0300

    Update mountdisk.sh

--- mountdisk.sh
+++ mountdisk.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 
+# Переменные
 DISK="/dev/nvme0n1"
 OPTS="noatime,compress=zstd:3,ssd,discard=async,space_cache=v2"
 
-echo "--- 1. Активация Swap ---"
-swapon "${DISK}p2"
+echo "--- 1. Проверка Swap ---"
+# Активируем, если еще не включен (ошибки скрываем)
+swapon "${DISK}p2" 2>/dev/null
 
-echo "--- 2. Монтирование корневого подтома (@) ---"
+echo "--- 2. Создание подтомов Btrfs ---"
+# Временный монтаж для создания структуры
+mount "${DISK}p3" /mnt
+btrfs subvolume create /mnt/@
+btrfs subvolume create /mnt/@home
+btrfs subvolume create /mnt/@snapshots
+btrfs subvolume create /mnt/@var
+btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@cache
+umount /mnt
+
+echo "--- 3. Монтирование системы ---"
+# Монтируем корень системы
 mount -o "$OPTS,subvol=@" "${DISK}p3" /mnt
 
-echo "--- 3. Создание точек монтирования ---"
+# Создаем точки монтирования
 mkdir -p /mnt/{boot,home,.snapshots,var,var/log,var/cache}
 
-echo "--- 4. Монтирование остальных разделов и подтомов ---"
-# Монтируем загрузчик (EFI)
+# Монтируем загрузочный раздел (EFI)
 mount "${DISK}p1" /mnt/boot
 
-# Монтируем подтома Btrfs
+# Монтируем остальные подтома
 mount -o "$OPTS,subvol=@home" "${DISK}p3" /mnt/home
 mount -o "$OPTS,subvol=@snapshots" "${DISK}p3" /mnt/.snapshots
 mount -o "$OPTS,subvol=@var" "${DISK}p3" /mnt/var
 mount -o "$OPTS,subvol=@log" "${DISK}p3" /mnt/var/log
 mount -o "$OPTS,subvol=@cache" "${DISK}p3" /mnt/var/cache
 
-echo "--- 5. Установка атрибутов ---"
+echo "--- 4. Настройка атрибутов (NoCoW) ---"
 # Отключаем Copy-on-Write для логов и кэша
 chattr +C /mnt/var/log
 chattr +C /mnt/var/cache
 
-echo "--- Готово! Диск примонтирован в /mnt ---"
-lsblk
+echo "--- Готово! Структура диска собрана в /mnt ---"
+lsblk "${DISK}"
