b0e9cac5be56f95ffc7eb7f48c633ef3b406eff4
kordiss-kds <720074@gmail.com>
2026-01-27 23:45:25 +0300

    Update postinst.sh

--- postinst.sh
+++ postinst.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "=== ПРЕДВАРИТЕЛЬНАЯ НАСТРОЙКА ==="
-
-# 1. Запрос данных с клавиатуры
+echo "=== 1. ПРЕДВАРИТЕЛЬНЫЕ ДАННЫЕ ==="
 read -p "Введите имя компьютера (hostname): " MY_HOSTNAME
 read -p "Введите имя пользователя: " MY_USER
 
@@ -12,7 +10,6 @@ TIMEZONE="Europe/Moscow"
 ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
 hwclock --systohc
 
-# Настройка локалей (два языка)
 sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/#ru_RU.UTF-8 UTF-8/ru_RU.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
@@ -24,26 +21,21 @@ echo "$MY_HOSTNAME" > /etc/hostname
 echo "KEYMAP=ruwin_alt_sh-UTF-8
 FONT=ter-v28b" > /etc/vconsole.conf
 
-echo "=== 3. УСТАНОВКА ВСЕХ ПАКЕТОВ ==="
+echo "=== 2. УСТАНОВКА ПАКЕТОВ (Драйверы, Звук, Утилиты) ==="
+pacman -S --noconfirm --needed \
+    sudo terminus-font zram-generator grub efibootmgr \
+    grub-btrfs inotify-tools snapper nvidia-open-cachyos \
+    nvidia-utils lib32-nvidia-utils amd-ucode pipewire \
+    pipewire-pulse pipewire-alsa pipewire-jack wireplumber \
+    bluez bluez-utils networkmanager udiskie udisks2 \
+    mc yazi gpm xdg-user-dirs p7zip unrar zip unzip \
+    lrzip lz4 mtools dosfstools squashfs-tools base-devel \
+    git bash-completion pacman-contrib vim neovim micro \
+    htop wget curl
 
-# Добавлен terminus-font для работы шрифта ter-v28b
-pacman -S --noconfirm \
-    sudo \
-    terminus-font \
-    zram-generator \
-    grub \
-    efibootmgr \
-    grub-btrfs \
-    inotify-tools \
-    networkmanager \
-    bluez \
-    bluez-utils \
-    udisks2 \
-    snapper
+echo "=== 3. НАСТРОЙКА СЕРВИСОВ И СИСТЕМЫ ==="
 
-echo "=== 4. НАСТРОЙКА СИСТЕМЫ ==="
-
-# Настройка ZRAM
+# ZRAM
 cat <<EOF > /etc/systemd/zram-generator.conf
 [zram0]
 zram-size = ram / 2
@@ -52,15 +44,20 @@ swap-priority = 100
 fs-type = swap
 EOF
 
-# Включение служб
+# Включение служб (без --now, так как мы в chroot)
 systemctl enable NetworkManager
 systemctl enable bluetooth
+systemctl enable gpm
+systemctl enable systemd-timesyncd
+systemctl enable paccache.timer
+systemctl enable grub-btrfsd
+systemctl enable nvidia-persistenced
 
-# Настройка GRUB
+# GRUB
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-# Настройка Snapper
+# Snapper
 umount /.snapshots || true
 rm -rf /.snapshots
 snapper -c root create-config /
@@ -68,23 +65,23 @@ btrfs subvolume delete /.snapshots
 mkdir /.snapshots
 mount -a
 
-echo "=== 5. ПОЛЬЗОВАТЕЛИ И ПАРОЛИ ==="
-
-# Настройка пароля root
-echo "Настройка пароля для ROOT:"
+echo "=== 4. ПОЛЬЗОВАТЕЛИ И ПАРОЛИ ==="
+echo "Пароль для ROOT:"
 passwd root
 
-# Создание пользователя и его пароля
 useradd -m -G wheel,storage,power -s /bin/bash "$MY_USER"
-echo "Настройка пароля для пользователя $MY_USER:"
+echo "Пароль для пользователя $MY_USER:"
 passwd "$MY_USER"
 
-# Разрешаем группе wheel использовать sudo
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
+# Настройка папок пользователя на английском (от имени пользователя)
+echo "Настройка XDG-папок на английском..."
+sudo -u "$MY_USER" LC_ALL=C xdg-user-dirs-update --force
+
 echo "-------------------------------------------------"
-echo "ВСЕ НАСТРОЕНО!"
-echo "Шрифт консоли: ter-v28b (Terminus)"
-echo "Переключение в TTY: Alt + Shift"
+echo "УСТАНОВКА ЗАВЕРШЕНА!"
 echo "-------------------------------------------------"
-echo "Выходите из системы: exit"
+echo "1. Введите 'exit'"
+echo "2. Введите 'umount -R /mnt'"
+echo "3. Введите 'reboot'"
